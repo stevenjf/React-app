0d8f64ececacb6f2451cbf17c5ff0ce89ca25162
Steven Fenton <steven-j-fenton@outlook.com>
2021-11-19 15:10:08 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,160 +1,105 @@
-import React, { useState } from "react";
 
-import Wrapper from ".components/Wrapper";
-import Screen from ".components/Screen";
-import ButtonBox from ".components/ButtonBox";
-import Button from ".components/button";
-
-const btnValues = [
-  ["C", "+-", "%", "/"],
-  [7, 8, 9, "X"],
-  [4, 5, 6, "-"],
-  [1, 2, 3, "+"],
-  [0, ".", "="],
-];
-
-const toLocaleString = (num) =>
-  String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
-
-const removeSpaces = (num) => num.toString().replace(/\s/g, "");
+import React from "react"
+import { useState } from "react"
+import { evaluate, sqrt, square  } from "mathjs"
+import "./App.css"
+// const math = create(all, {})
 
 const App = () => {
-  let [calc, setCalc] = useState({
-    sign: "",
-    num: 0,
-    res: 0,
-  });
+  const [buttons] = useState(["clear", "back", "square", "sqrt", 9, 8, 7, 6, 5, 4, 3, 2, 1,0, "-", "*", "+", ".", "/", "="])
+  const [output, setOutput] = useState([0])
+  const [sum, setSum] = useState([])
 
-  const numClickHandler = (e) => {
-    e.preventDefault();
-    const value = e.target.innerHTML;
+  
 
-    if (removeSpaces(calc.num).length < 16) {
-      setCalc({
-        ...calc,
-        num:
-          calc.num === 0 && value === "0"
-            ? "0"
-            : removeSpaces(calc.num) % 1 === 0
-            ? toLocaleString(Number(removeSpaces(calc.num + value)))
-            : toLocaleString(calc.num + value),
-        res: !calc.sign ? 0 : calc.res,
-      });
+  const clickHandler = (index, e) => {
+    
+    if (e.target.innerHTML === "=" && output) {
+      equalsHandler() }
+    else if ( e.target.innerHTML === "square" ){
+      squareHandler()
+    } else if (e.target.innerHTML === "sqrt"){
+      sqrtHandler()
+    } else if ( e.target.innerHTML === "clear"){
+      screenClear()
+    } else if (e.target.innerHTML === "back"){
+      const storedOutput = [...output]
+      storedOutput.pop()
+      setOutput(storedOutput)
+      const storedSum = [...sum]
+      storedSum.pop()
+      setSum(storedSum)
+    } else if (output === "error") {
+      setOutput("error")
     }
-  };
+    else {
+      
+      const storedOutput = [...output]
+      storedOutput.push(buttons[index])
+      setOutput(storedOutput)
 
-  const commaClickHandler = (e) => {
-    e.preventDefault();
-    const value = e.target.innerHTML;
-
-    setCalc({
-      ...calc,
-      num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
-    });
-  };
-
-  const signClickHandler = (e) => {
-    e.preventDefault();
-    const value = e.target.innerHTML;
-
-    setCalc({
-      ...calc,
-      sign: value,
-      res: !calc.res && calc.num ? calc.num : calc.res,
-      num: 0,
-    });
-  };
-
-  const equalsClickHandler = () => {
-    if (calc.sign && calc.num) {
-      const math = (a, b, sign) =>
-        sign === "+"
-          ? a + b
-          : sign === "-"
-          ? a - b
-          : sign === "X"
-          ? a * b
-          : a / b;
-
-      setCalc({
-        ...calc,
-        res:
-          calc.num === "0" && calc.sign === "/"
-            ? "Can't divide with 0"
-            : toLocaleString(
-                math(
-                  Number(removeSpaces(calc.res)),
-                  Number(removeSpaces(calc.num)),
-                  calc.sign
-                )
-              ),
-        sign: "",
-        num: 0,
-      });
+      const storedSum = [...sum]
+      storedSum.push(buttons[index])
+      setSum(storedSum)
     }
-  };
+  }
+  const squareHandler = () => {
+    if (output.length >0) {
+      const storedOutput = [...output]
+      const joinedOutput = storedOutput.join("")
+      const answer = square(joinedOutput)
+      const strans = answer.toString()
+      const splitans = strans.split("")
+      
+      setOutput(splitans)
+    }
+    
+  }
+  const sqrtHandler = () => {
+    if (output.length >0) {
+      const storedOutput = [...output]
+      const joinedOutput = storedOutput.join("")
+      const answer = sqrt(joinedOutput)
+      const strans = answer.toString()
+      const splitans = strans.split("")
+      setOutput(splitans)
+    }
+  }
+  const screenClear = () => {
+    setOutput("")
+    setSum("")
+  }
+  
+  const equalsHandler = () => {
+    if (output.length > 1) {
+      const storedOutput = [...output]
+      const joinedOutput = storedOutput.join("")
+      const answer = evaluate(joinedOutput)
+      const strans = answer.toString()
+      const splitans = strans.split("")
+      setOutput(splitans)
 
-  const invertClickHandler = () => {
-    setCalc({
-      ...calc,
-      num: calc.num ? toLocaleString(removeSpaces(calc.num) * -1) : 0,
-      res: calc.res ? toLocaleString(removeSpaces(calc.res) * -1) : 0,
-      sign: "",
-    });
-  };
-
-  const percentClickHandler = () => {
-    let num = calc.num ? parseFloat(removeSpaces(calc.num)) : 0;
-    let res = calc.res ? parseFloat(removeSpaces(calc.res)) : 0;
-
-    setCalc({
-      ...calc,
-      num: (num /= Math.pow(100, 1)),
-      res: (res /= Math.pow(100, 1)),
-      sign: "",
-    });
-  };
-
-  const resetClickHandler = () => {
-    setCalc({
-      ...calc,
-      sign: "",
-      num: 0,
-      res: 0,
-    });
-  };
+      const storedSum = [...sum]
+      storedSum.push(")")
+      storedSum.unshift("(")
+      setSum(storedSum)
+    }
+  }
 
   return (
-    <Wrapper>
-      <Screen value={calc.num ? calc.num : calc.res} />
-      <ButtonBox>
-        {btnValues.flat().map((btn, i) => {
-          return (
-            <Button
-              key={i}
-              className={btn === "=" ? "equals" : ""}
-              value={btn}
-              onClick={
-                btn === "C"
-                  ? resetClickHandler
-                  : btn === "+-"
-                  ? invertClickHandler
-                  : btn === "%"
-                  ? percentClickHandler
-                  : btn === "="
-                  ? equalsClickHandler
-                  : btn === "/" || btn === "X" || btn === "-" || btn === "+"
-                  ? signClickHandler
-                  : btn === "."
-                  ? commaClickHandler
-                  : numClickHandler
-              }
-            />
-          );
-        })}
-      </ButtonBox>
-    </Wrapper>
-  );
-};
+    <div className="calculator">
+      <div className="screen"> {output} <div className="sum"> {sum}</div> </div>
+      
+      <div className="container">
 
-export default App;
+        {buttons.map((button, index) => {
+          return <button className="btn" key={index} onClick={(e) => clickHandler(index, e)}>{button}</button>
+
+        })}
+        
+      </div>
+      </div>
+   
+  )}
+
+  export default App;
--- src/components/ButtonBox.js
+++
@@ -1,9 +0,0 @@
-import "./ButtonBox.css";
-
-const ButtonBox = ({ children }) => {
-  return <div className="buttonBox">{children}</div>;
-};
-
-export default ButtonBox;
-
-
